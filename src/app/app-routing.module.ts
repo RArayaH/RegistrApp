e01d349409756200ed8c9b63ac2d111327aa3ce1
RArayaH <81941016+RArayaH@users.noreply.github.com>
2021-12-06 02:23:15 -0300

    'casi listos pa la apk'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
-    {
+  {
+    path: '',
+    redirectTo: '/login', 
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
   },
